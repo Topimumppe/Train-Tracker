a386c8324795150c7da0e9cd379dc76e86068c6e
Topimumppe <topheikki@gmail.com>
2025-03-20 19:55:39 +0200

    Initial commit

---
+++ Project/train-tracker/src/API.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// Function to fetch train locations from the DigiTraffic API
+const fetchTrainLocations = async () => {
+    try {
+        const response = await axios.get('https://rata.digitraffic.fi/api/v1/train-locations.geojson/latest');
+        console.log('Train data fetched successfully:', response.data);
+        return response.data; // Return the GeoJSON data
+    } catch (error) {
+        console.error('Error fetching train data:', error);
+        throw error; // Throw the error to handle it in the calling function
+    }
+};
+
+export default fetchTrainLocations;
